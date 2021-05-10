434d76efd67d2384c32ca2e554a0a8c429ca1eb1
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-05-10 14:40:34 -0500

    added Nav w/ Links

---
+++ blog/src/components/Nav.jsx
@@ -0,0 +1,12 @@
+function Nav() {
+  return (
+    <main>
+      <Link to="/">Homepage</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact Me</Link>
+      <Link to="/post">New Post</Link>
+    </main>
+  )
+}
+
+export default Nav;
