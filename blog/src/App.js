ea32240bba2c4fc8f27260015b372d4e8576d52d
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-05-10 14:58:54 -0500

    working through the api call

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,7 +1,25 @@
+import Nav from "./components/Nav";
+import { Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Route exact path="/">
+        <main>Home</main>
+      </Route>
+      <Route path="/about">
+        <main>About Me</main>
+      </Route>
+      <Route path="/contact">
+        <main>Contact</main>
+      </Route>
+      <Route path="/post">
+        <main>Post</main>
+      </Route>
+    </div>
+  );
 }
 
 export default App;
