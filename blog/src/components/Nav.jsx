f6a050246a7c9fb340b1c56d7525eba816b7a4aa
Paul Oh <pauloh@Pauls-MBP.attlocal.net>
2021-05-13 00:36:30 -0500

    added new nav bar

--- blog/src/components/Nav.jsx
+++ blog/src/components/Nav.jsx
@@ -3,18 +3,29 @@ import { Link } from "react-router-dom"
 function Nav() {
   return (
     <nav className="navbar">
-      <Link to='/'>
-        <button className="nav-home">Home</button>
-      </Link>
-      <Link to='/new'>
-        <button className="nav-post">New Post</button>
-      </Link>
-      <Link to='/about'>
-        <button className="nav-about">About</button>
-      </Link>
-      <Link to='/contact'>
-        <button className="nav-contact">Contact</button>
-      </Link>
+      <a className="brand" href="#">paul</a>
+      <ul>
+        <li>
+          <Link to='/'>
+            <a href="#" className="nav-home">Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link to='/new'>
+            <a href="#" className="nav-post">New Post</a>
+          </Link>
+        </li>
+        <li>
+          <Link to='/about'>
+            <a href="#" className="nav-about">About</a>
+          </Link>
+        </li>
+        <li>
+          <Link to='/contact'>
+            <a href="#" className="nav-contact">Contact</a>
+          </Link>
+        </li>
+      </ul>
     </nav>
   )
 }
